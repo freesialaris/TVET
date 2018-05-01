20c842e56d63258bef56b85208d4004b5f9a3f96
freesialaris <freesiaisepic@gmail.com>
2018-05-01 15:11:21 +1000

    added visualstudio project

---
+++ Unity/Assets/~GoneHome/Scripts/Goal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events; 
+
+public class Goal : MonoBehaviour {
+
+    public UnityEvent onGoal;
+    void OnTriggerEnter(Collider other)
+    {
+        //check if onGoal event exists
+        if(onGoal != null)
+        {
+            //invoke event
+            onGoal.Invoke();
+        }
+    }
+		
+	}
+
