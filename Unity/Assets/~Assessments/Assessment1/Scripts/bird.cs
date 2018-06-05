756fd087398e95b67634ed74c4402a5e085f844f
freesialaris <freesiaisepic@gmail.com>
2018-06-05 15:56:33 +1000

    flappey bird

---
+++ Unity/Assets/~Assessments/Assessment1/Scripts/bird.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class bird : MonoBehaviour
+{
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    void start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+    
+}
---
+++ Unity/Assets/~Assessments/Assessment1/Scripts/raycastPlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class raycastPlay : MonoBehaviour {
+
+    void Update()
+    {
+        if (!Input.GetMouseButton(0))
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        ScreenMouseRay();
+
+    }
+    public void ScreenMouseRay()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 5;
+
+        Vector2 v = Camera.main.ScreenToWorldPoint(mousePosition);
+        Collider2D[] col = Physics2D.OverlapPointAll(v);
+        if (col.Length > 0)
+        {
+            foreach (Collider2D c in col)
+            {
+                if (c.CompareTag("Button") && !Input.GetMouseButton(0))
+                {
+                    c.GetComponent<SpriteRenderer>().color = Color.grey;
+                }
+                if (Input.GetMouseButtonDown(0) && c.CompareTag("Button"))
+                {
+                    Debug.Log("Play");
+                    c.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+    }
+}
