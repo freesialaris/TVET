800ec9bce6de42fa53db1851d9d0aba7ff42566e
Freesia Laris <freesiaisepic@gmail.com>
2018-06-10 21:26:12 +1000

    hhHHHHHH

---
+++ Unity/Assets/~Assessments/Assessment1/Scripts/tapController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class tapController : MonoBehaviour
+{
+    public float tapForce = 30;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+    public GameObject bird;
+
+    Rigidbody2D rigid;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    void start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        bird = GameObject.Find("Bird");
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            bird.transform.rotation = forwardRotation;
+            rigid.AddForce(Vector3.up * tapForce, ForceMode2D.Force);
+
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Score")
+        {
+            // score event
+        }
+
+        if (col.gameObject.tag == "Deadzone")
+        {
+            // end game, go to menu screen
+            SceneManager.LoadScene("Menu");
+
+        }
+    }
+}
